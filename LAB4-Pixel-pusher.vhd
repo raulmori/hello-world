0fb39449a4192ddadb2314ef8044694c624d96e9
raulmori <47406865+raulmori@users.noreply.github.com>
2019-04-08 12:16:53 -0400

    Update LAB4-Pixel-pusher.vhd

--- LAB4-Pixel-pusher.vhd
+++ LAB4-Pixel-pusher.vhd
@@ -6,23 +6,24 @@ use ieee.numeric_std.all;
 entity pixel_pusher is
       Port (
             clk, en, vs, vid : in std_logic;  
-            pixel: in std_logic_vector (7 downto 0);
-            hcount: in std_logic_vector (9 downto 0);
-            R, B: out std_logic_vector (4 downto 0);
-            G: out std_logic_vector (5 downto 0);
-            addr: out std_logic_vector (17 downto 0));
+            pixel:      in std_logic_vector (7 downto 0);
+            hcount:     in std_logic_vector (9 downto 0);
+            
+            R, B:       out std_logic_vector (4 downto 0);
+            G:          out std_logic_vector (5 downto 0);
+            addr:       out std_logic_vector (17 downto 0));
 end pixel_pusher;
 
 
 architecture rtl of pixel_pusher is
-        signal addrSig : unsigned(17 downto 0) := (others => '0');
+        signal addrSig : unsigned(17 downto 0) := (others => '0');     --This is a temporary signal
         
         begin
-            addr <= std_logic_vector(addrSig);
+            addr <= std_logic_vector(addrSig);        
             
 -------------------------------------------------------------------------------------                
             
-            process(clk)begin
+            process(clk)begin       --This is the first process
                 if(rising_edge(clk)) then if (vs = '0') then
                         addrSig <= (others => '0');
                 elsif (en = '1' and hcount < "0111100000" and vid = '1') then -- 480 = "0111100000"
@@ -31,7 +32,7 @@ architecture rtl of pixel_pusher is
                 
 -------------------------------------------------------------------------------------                
                 
-            process(clk)begin
+            process(clk)begin       --This is the second process
                 if(rising_edge(clk)and en ='1' ) then
                       if( vid = '1' and hcount < "0111100000") then -- 480 = "0111100000"
                            R <= pixel(7 downto 5) & "00";G <= pixel(4 downto 2) & "000";B <= pixel(1 downto 0) & "000";
