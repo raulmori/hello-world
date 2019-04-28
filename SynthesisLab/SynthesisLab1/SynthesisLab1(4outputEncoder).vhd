90e319ab345a75f0f6c01c8137fee0ca304694d3
raulmori <47406865+raulmori@users.noreply.github.com>
2019-04-28 07:31:18 -0400

    Update and rename SynthesisLab1(4input6outEncoder).vhd to SynthesisLab1(4outputEncoder).vhd

---
+++ SynthesisLab/SynthesisLab1/SynthesisLab1(4outputEncoder).vhd
@@ -0,0 +1,23 @@
+--Here we use "CONDITIONAL-SIGNAL-ASSIGNMENT"
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+
+entity encoder4bits is      -- entity
+       port ( 
+             D_IN : in std_logic_vector(3 downto 0);
+             SZ_OUT : out std_logic_vector(1 downto 0)
+             );
+end encoder4bits;
+  
+  
+architecture Behavioral of encoder4bits is
+      begin
+          SZ_OUT <=
+                  "00" when (D_IN = "0001") else
+                  "01" when (D_IN = "0010") else
+                  "10" when (D_IN = "0100") else
+                  "11" when (D_IN = "1000") else
+                      "0000" ;         
+end Behavioral;
