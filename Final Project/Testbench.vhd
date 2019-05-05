392bf643fd41d36e58a072688b600f9d96ccb24c
raulmori <47406865+raulmori@users.noreply.github.com>
2019-05-05 07:40:02 -0400

    Update Testbench.vhd

--- Final Project/Testbench.vhd
+++ Final Project/Testbench.vhd
@@ -31,7 +31,7 @@ ARCHITECTURE behavior OF tb_car_parking_system_VHDL IS
         signal reset : std_logic := '0';
         signal front_sensor : std_logic := '0';
         signal back_sensor : std_logic := '0';
-        signal pass : std_logic_vector(1 downto 0) := (others => '0');
+        signal pass : std_logic_vector(3 downto 0) := (others => '0');
       
         signal GREEN_LED : std_logic;              --Outputs
         signal RED_LED : std_logic;
@@ -58,30 +58,28 @@ ARCHITECTURE behavior OF tb_car_parking_system_VHDL IS
                   wait for clk_period/2;
                   clk <= '1';
                   wait for clk_period/2;
-                   end process;
-                   -- Stimulus process
-                   stim_proc: process
+              end process;
+                   
+              stim_proc: process       -- Stimulus process
                    begin  
-                      reset <= '0';
-                  front_sensor <= '0';
-                  back_sensor <= '0';
-                  Pass <= "0011";
-                      wait for clk_period*10;
-                  reset <= '1';
-                  wait for clk_period*10;
-                  front_sensor <= '1';
-                  wait for clk_period*10;
-                  Pass <= "0011";
-                  Pass <= "0011";
-                  back_sensor <= '1';
-                  Pass <= "0011";
-                  front_sensor <= '0';
-                  Pass <= "0011";
-                  back_sensor <= '1';
-                  back_sensor <= '0';
-                      -- insert your stimulus here 
-                
+                      reset <= '1';
+                      front_sensor <= '0';
+                      back_sensor <= '0';
+                      Pass <= "0011";
+                             wait for clk_period*10;
+                      reset <= '1';
+                            wait for clk_period*10;
+                      front_sensor <= '1';
+                             wait for clk_period*10;
+                      Pass <= "0011";
+                      back_sensor <= '1';
+                      
+                      front_sensor <= '0';
+                      Pass <= "0011";
+                      back_sensor <= '1';
+                      back_sensor <= '0';
+                          -- insert your stimulus here 
                       wait;
-                   end process;
+              end process;
 
 END;
